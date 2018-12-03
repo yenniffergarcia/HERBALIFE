229c79cc840a849e51e16f5d03b455c34d646f7c
yenniffergarcia <41272785+yenniffergarcia@users.noreply.github.com>
2018-12-02 20:53:19 -0600

    dashboard new
    edicion en dashboard sin probar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Factura;
+use App\Stock;
+use App\Bonifciacion;
+use App\Regalia;
+use App\Persona;
 use Auth;
 
 class HomeController extends Controller
@@ -14,6 +19,25 @@ class HomeController extends Controller
         
     public function index()
     {
-        return view('home');
+    	$pedidos = Factura::join('persona', 'pedido.fkpersona', 'persona.id')
+    		->join('persona_nivel', 'pedido.fkpersonivel', 'persona_nivel.id')
+    		->join('nivel', 'persona_nivel.fknivel', 'nivel.id')
+    		->where('pedido.fkpersona', Auth::user()->fkpersona)->where('pedido.estado', 1)
+    		->select('pedido.*', 'nombre1', 'apellido1', 'codigo', 'nivel.nombre as nivel')->take(20)->get();
+
+    	$stocks = Stock::join('producto', 'stock.fkproducto', 'producto.id')
+    		->where('stock.fkpersona', Auth::user()->fkpersona)
+    		->select('stock.*', 'nombre', 'punto', 'precio')->take(20)->get();
+
+    	$redes = Persona::where('id_padre', Auth::user()->fkpersona)
+    		->select('codigo', \DB::raw("(SELECT p.punto FROM punto_mes p WHERE p.fkpersona = ".'persona.id'." AND p.fkmes = ".date('n')." AND YEAR(p.fecha) = ".date('Y').") as punteo_mes"), \DB::raw("(SELECT p.punto FROM punto_mes p WHERE p.fkpersona = ".'persona.id'." AND YEAR(p.fecha) = ".date('Y')." ORDER BY p.id DESC LIMIT 2 ) as punteo_dos_meses"), \DB::raw("(SELECT p.punto FROM punto_mes p WHERE p.fkpersona = ".'persona.id'." AND YEAR(p.fecha) = ".date('Y')." ORDER BY p.id DESC LIMIT 12 ) as punteo_doce_meses"))->get();
+
+    	$puntos = Persona::where('id', Auth::user()->fkpersona)
+    		->select(\DB::raw("(SELECT p.punto FROM punto_mes p WHERE p.fkpersona = ".'persona.id'." AND p.fkmes = ".date('n')." AND YEAR(p.fecha) = ".date('Y').") as punteo_mes"), \DB::raw("(SELECT p.punto FROM punto_mes p WHERE p.fkpersona = ".'persona.id'." AND YEAR(p.fecha) = ".date('Y')." ORDER BY p.id DESC LIMIT 2 ) as punteo_dos_meses"), \DB::raw("(SELECT p.punto FROM punto_mes p WHERE p.fkpersona = ".'persona.id'." AND YEAR(p.fecha) = ".date('Y')." ORDER BY p.id DESC LIMIT 12 ) as punteo_doce_meses"))->get();
+
+    	$informacion_pagos = Persona::where('id', Auth::user()->fkpersona)
+    		->select('codigo', 'nombre1', 'apellido1', \DB::raw("(SELECT SUM(b.monto) FROM bonificacion b WHERE b.fkpersona = ".'persona.id'." AND b.anio = ".date('Y').") as bonificacion"), \DB::raw("(SELECT SUM(r.monto) FROM regalia r WHERE r.fkpersona = ".'persona.id'." AND r.anio = ".date('Y').") as regalia"), \DB::raw("(SELECT SUM(p.total-p.subtotal) FROM pedido p WHERE p.fkpersona = ".'persona.id'." AND YEAR(p.fecha) = ".date('Y').") as descuento"), \DB::raw("(SELECT SUM(pm.punto) FROM punto_mes pm INNER JOIN persona p ON p.id = pm.fkpersona WHERE p.id_padre = ".'persona.id'." AND YEAR(pm.fecha) = ".date('Y').") as red"))->get();
+
+        return view('home', compact('pedidos', 'stocks', 'redes', 'puntos', 'informacion_pagos'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,270 @@
 @section('title', 'HERBALIFE')
 
 @section('content_header')
-
+	<h1>Dashboard</h1>
 @stop
 
 @section('content')
+
+	<div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-aqua">
+            <div class="inner">
+            	<h3>150</h3>
+            	<p>Regalias</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ver más información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-green">
+            <div class="inner">
+            	<h3>53<sup style="font-size: 20px">%</sup></h3>
+            	<p>Bonificación actual</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ver más información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>{{ count($redes) }}</h3>
+              <p>Asociados en Red</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="{{ route('Persona.index') }}" class="small-box-footer">Ver más información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>65</h3>
+              <p>Pagos Obtenidos</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ver más información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+    </div>
+
+
+	<div class="row">
+        <div class="col-md-12">
+          	<div class="box">
+            	<div class="box-header with-border">
+              		<h3 class="box-title">Información Mensual</h3>
+            	</div>
+
+	            <div class="box-body">
+	            	<div class="row">
+	                	<div class="col-md-8">
+	                  		<p class="text-center">
+	                    		<strong>{{ date('d-m-Y h:m:s') }}</strong>
+	                  		</p>
+
+				            <div class="table-responsive">
+				                <table class="table no-margin">
+					                <thead>
+						                <tr>
+							                <th>Código</th>
+							                <th>Puntos 1 Mes</th>
+							                <th>Puntos 2 Meses</th>
+							                <th>Puntos 12 Meses</th>
+						                </tr>
+					                </thead>
+					                <tbody>
+					                	@foreach($redes as $red)
+						                <tr>
+							                <td><a href="#">{{ $red->codigo }}</a></td>
+							                <td>{{ $red->punteo_mes }}</td>
+											<td>{{ $red->punteo_dos_meses }}</td>
+											<td>{{ $red->punteo_doce_meses }}</td> 
+						                </tr>
+						                @endforeach
+					                </tbody>
+				                </table>
+				            </div>
+
+	                	</div>
+
+	                	<div class="col-md-4">
+			                <p class="text-center">
+			                	<strong>Punteo por Nivel</strong>
+			                </p>
+
+			                <div class="progress-group">
+			                    <span class="progress-text">Consultor Mayor</span>
+			                    @foreach($puntos as $punto)
+				                    <span class="progress-number"><b>{{ $punto->punteo_mes }}</b>/500</span>
+				                    <div class="progress sm">
+				                      <div class="progress-bar progress-bar-aqua" style="width: {{ ($punto->punteo_mes/500)*100 }}%"></div>
+				                    </div>
+			                    @endforeach
+			                </div>
+	                  
+			                <div class="progress-group">
+			                    <span class="progress-text">Constructor del Exito</span>
+			                    @foreach($puntos as $punto)
+				                    <span class="progress-number"><b>{{ $punto->punteo_dos_meses }}</b>/2500</span>
+				                    <div class="progress sm">
+				                      <div class="progress-bar progress-bar-red" style="width: {{ ($punto->punteo_dos_meses/2500)*100 }}%"></div>
+				                    </div>
+			                    @endforeach
+			                </div>
+
+			                <div class="progress-group">
+			                    <span class="progress-text">Mayorista</span>
+			                    @foreach($puntos as $punto)
+				                    <span class="progress-number"><b>{{ $punto->punteo_doce_meses }}</b>/4000</span>
+				                    <div class="progress sm">
+				                      <div class="progress-bar progress-bar-green" style="width: {{ ($punto->punteo_doce_meses/4000)*100 }}%"></div>
+				                    </div>
+			                    @endforeach
+			                </div>
+
+	                	</div>
+	              	</div>
+	            </div>
+
+	            <div class="box-footer">
+	            	@foreach($informacion_pagos as $informacion)
+		            	<div class="row">
+		            		<div class="col-sm-12" style="text-align: center;">
+		            			<h3>Información Anual - <b>{{ date('Y') }}</b></h3>
+		            			<h4>{{ $informacion->codigo }} - {{ $informacion->nombre1 }} {{ $informacion->apellido1 }}</h4>
+		            		</div>
+		            	</div>
+		                <div class="row">
+		                	<div class="col-sm-3 col-xs-6">
+		                		<div class="description-block border-right">
+			                		<h5 class="description-header">Q {{ $informacion->bonificacion }}</h5>
+			                		<span class="description-text">TOTAL BONIFICACIONES</span>
+		                		</div>
+		                	</div>
+
+			                <div class="col-sm-3 col-xs-6">
+			                	<div class="description-block border-right">
+				                    <h5 class="description-header">Q {{ $informacion->regalia }}</h5>
+				                    <span class="description-text">TOTAL REGALIAS</span>
+			                	</div>
+			                </div>
+
+			                <div class="col-sm-3 col-xs-6">
+			                	<div class="description-block border-right">
+				                    <h5 class="description-header">Q {{ $informacion->descuento }}</h5>
+				                    <span class="description-text">TOTAL DESCUENTOS</span>
+			                	</div>
+			                </div>
+
+			                <div class="col-sm-3 col-xs-6">
+				                <div class="description-block">
+				                    <h5 class="description-header">Pts. {{ $informacion->red }}</h5>
+				                    <span class="description-text">PUNTOS DE LA RED</span>
+				                </div>
+			                </div>
+		              	</div>
+		            @endforeach
+	            </div>
+        	</div>
+        </div>
+    </div>
+
+
+	<div class="row">
+		
+		<div class="col-xs-8">
+			
+			<div class="box box-info">
+	            <div class="box-header with-border">
+	              <h3 class="box-title">Pedidos Recibidos</h3>
+	            </div>
+
+	            <div class="box-body">
+		            <div class="table-responsive">
+		                <table class="table no-margin">
+			                <thead>
+				                <tr>
+					                <th>Pedido #</th>
+					                <th>¿Quien realizo el Pedido?</th>
+					                <th>Sub Total</th>
+					                <th>Total</th>
+					                <th>Pedido</th>
+				                </tr>
+			                </thead>
+			                <tbody>
+			                	@foreach($pedidos as $pedido)
+				                <tr>
+					                <td><a href="#">{{ $pedido->id }}</a></td>
+					                <td>{{ $pedido->codigo }} - {{ $pedido->nombre1 }} {{ $pedido->apellido1 }}</td>
+									<td>{{ $pedido->subtotal }}</td>
+									<td>{{ $pedido->total }}</td>
+									@if($pedido->pagado == 1)
+										<td><span class="label label-success">Pagado</span></td>
+									@else
+										<td><span class="label label-danger">No Pagado</span></td>
+									@endif  
+				                </tr>
+				                @endforeach
+			                </tbody>
+		                </table>
+		            </div>
+	            </div>
+	            <div class="box-footer clearfix">
+	            	<a href="{{ route('Pedido.index') }}" class="btn btn-sm btn-default btn-flat pull-right">Ver todos los pedidos...</a>
+	            </div>
+	        </div>
+
+		</div>
+
+	<div class="col-xs-4">
+			
+			<div class="box box-info">
+	            <div class="box-header with-border">
+	              <h3 class="box-title">Stock</h3>
+	            </div>
+
+	            <div class="box-body">
+		            <div class="table-responsive">
+		                <table class="table no-margin">
+			                <thead>
+				                <tr>
+					                <th>Cantidad</th>
+					                <th>Producto</th>
+					                <th>Precio</th>
+				                </tr>
+			                </thead>
+			                <tbody>
+			                	@foreach($stocks as $stock)
+				                <tr>
+					                <td>{{ $stock->cantidad }}</td>
+					                <td>{{ $stock->nombre }} - {{ $stock->punto }}</td>
+					                <td>{{ $stock->precio }}</td>
+				                </tr>
+				                @endforeach
+			                </tbody>
+		                </table>
+		            </div>
+	            </div>
+	            <div class="box-footer clearfix">
+	            	<a href="{{ route('Stock.index') }}" class="btn btn-sm btn-default btn-flat pull-right">Ver todos los pedidos...</a>
+	            </div>
+	        </div>
+
+		</div>		
+
+	</div>
 
 @stop
